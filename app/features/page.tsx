2102193f02dd37c9a6d4ebb2d1ee12b216582377
Utkarsh <utkarshjha.4009@gmail.com>
2024-08-28 22:49:39 +0530

    Feature page

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -5,9 +5,9 @@ import ShadCard from '../components/shadCard'
 const page = () => {
   return (
     <>
-    <h1 className='font-semibold text-4xl ml-[20px] m-2'>Features</h1>
+    <h1 className='font-semibold text-4xl ml-[22px] m-2 mt-[20px]'>Features</h1>
     <div className="flex">
-    <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 p-2 max-w-[900px]'>
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 sm:p-5 p-6 max-w-[900px]'>
         {featureData.map((feature, index) => (
         <ShadCard key={index} title = {feature.title} description={feature.description} content={feature.content}/>
       ))}
