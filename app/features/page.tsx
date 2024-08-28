967d28dbdaf5ccd3afa9fe364ab4cab8a3bf3c2f
Utkarsh <utkarshjha.4009@gmail.com>
2024-08-28 23:34:15 +0530

    footer

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -6,7 +6,7 @@ import Footer from '../components/Footer'
 const page = () => {
   return (
     <>
-    <div className='min-h-screen'>
+    <div className='min-h-screen mb-[100px]'>
     <h1 className='font-semibold text-4xl ml-[25px] m-2 mt-[20px]'>Features</h1>
     <div className="flex">
     <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 sm:p-5 p-6 max-w-[1000px]'>
