1102de97ed5f944373f7812aac0f4de533006e89
Utkarsh <utkarshjha.4009@gmail.com>
2024-08-28 22:30:58 +0530

    Fixed build errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     <div>
       <h1 className='font-semibold text-4xl ml-[20px] m-2'>About</h1>
       <p className='ml-[20px] max-w-[900px] mt-[20px] m-2'>
-        Briefbuzz is your go-to platform for staying informed with just the right amount of detail. We understand that everyone’s time is valuable, which is why we offer news summaries in varying lengths—200, 350, 500, and 1000 words—powered by Google's cutting-edge Gemini AI. Whether you’re looking for a quick overview or an in-depth analysis, Briefbuzz has you covered.
+        Briefbuzz is your go-to platform for staying informed with just the right amount of detail. We understand that everyone&#39;s time is valuable, which is why we offer news summaries in varying lengths—200, 350, 500, and 1000 words—powered by Google's cutting-edge Gemini AI. Whether you&#39;re looking for a quick overview or an in-depth analysis, Briefbuzz has you covered.
         Our mission is to provide accurate, concise, and easily digestible news content, so you can stay informed without the overwhelm. With a clean, user-friendly interface and the ability to customize your news reading experience, Briefbuzz ensures that staying updated is both easy and enjoyable.
 
         Join us in revolutionizing the way you consume news—efficiently, intelligently, and on your terms.
