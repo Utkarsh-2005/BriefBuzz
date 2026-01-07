3a61150b619397dfa96d8630c6a4535fe8f50d2d
Utkarsh <utkarshjha.4009@gmail.com>
2026-01-07 20:17:09 +0530

    changed model to gemini-2.5-flash-lite

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -17,7 +17,7 @@ if (!apiKey) {
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.5-flash-lite",
 });
 
 const generationConfig = {
